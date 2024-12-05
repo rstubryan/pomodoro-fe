746b7013bfed40a3d6e91f8ac0b67db620c19138
restubryanr <restubumirr@gmail.com>
2024-12-05 21:46:34 +0700

    feat(dashboard): add dashboard layout component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,9 @@
-import { cookies } from "next/headers";
-import Logout from "@/app/dashboard/logout";
+import DashboardLayout from "@/components/templates/DashboardLayout";
 
-export default async function Dashboard() {
-  const cookie = (await cookies()).get("user")?.value;
-  const authenticatedData = cookie ? JSON.parse(cookie) : null;
-
+export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {authenticatedData ? (
-        <pre>
-          {JSON.stringify(authenticatedData, null, 2)}
-          <Logout />
-        </pre>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <>
+      <DashboardLayout />
+    </>
   );
 }
---
+++ src/components/templates/DashboardLayout.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/molecules/Navbar";
+
+export default function DashboardLayout() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
