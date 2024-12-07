e9a3d7cc2282f03b504f6841ba389077ee38ec46
restubryanr <restubumirr@gmail.com>
2024-12-07 21:47:47 +0700

    feat(navbar): conditionally render login/logout buttons

--- src/components/molecules/Navbar/index.tsx
+++ src/components/molecules/Navbar/index.tsx
@@ -62,38 +62,48 @@ export default function Navbar() {
             <SubHeading>Workpace</SubHeading>
           </Link>
 
-          <ul className={`flex items-center gap-2`}>
-            {navLink.map((item) => (
-              <li key={item.id} className={`ml-4`}>
-                <Link
-                  href={item.link}
-                  className={`${item.isActive ? "underline underline-offset-8" : ""}`}
-                >
-                  {item.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          {pathname.startsWith("/dashboard") ? (
+            <ul className={`hidden items-center gap-2 lg:flex`}>
+              {navLink.map((item) => (
+                <li key={item.id} className={``}>
+                  <Link
+                    href={item.link}
+                    className={`${item.isActive ? "underline underline-offset-8" : ""}`}
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : null}
         </div>
 
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant={`ghost`}>
-              Hi, {username}!
-              <ChevronDown size={12} />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              onClick={handleLogout}
-              className={`cursor-pointer`}
-            >
-              Logout
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        {authenticatedData ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant={`ghost`}>
+                Hi, {username}!
+                <ChevronDown size={12} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={handleLogout}
+                className={`cursor-pointer`}
+              >
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <Button asChild>
+            <Link href={`/login`} className={`cursor-pointer`}>
+              Login
+            </Link>
+          </Button>
+        )}
       </nav>
     </>
   );
