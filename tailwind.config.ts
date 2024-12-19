f2d6036cb22e5fc49926c4d31aa0d86e98dcd7d1
restubryanr <restubumirr@gmail.com>
2024-12-19 13:46:15 +0700

    chore(deps): update radix-ui dependencies and add separator component. update cva and add global font styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,13 +11,13 @@ export default {
     extend: {
       container: {
         center: true,
-        padding: {
-          DEFAULT: "1rem",
-          sm: "2rem",
-          lg: "4rem",
-          xl: "5rem",
-          "2xl": "6rem",
-        },
+        // padding: {
+        //   DEFAULT: "1rem",
+        //   sm: "2rem",
+        //   lg: "4rem",
+        //   xl: "5rem",
+        //   "2xl": "6rem",
+        // },
       },
       colors: {
         background: "hsl(var(--background))",
@@ -59,6 +59,16 @@ export default {
           "3": "hsl(var(--chart-3))",
           "4": "hsl(var(--chart-4))",
           "5": "hsl(var(--chart-5))",
+        },
+        sidebar: {
+          DEFAULT: "hsl(var(--sidebar-background))",
+          foreground: "hsl(var(--sidebar-foreground))",
+          primary: "hsl(var(--sidebar-primary))",
+          "primary-foreground": "hsl(var(--sidebar-primary-foreground))",
+          accent: "hsl(var(--sidebar-accent))",
+          "accent-foreground": "hsl(var(--sidebar-accent-foreground))",
+          border: "hsl(var(--sidebar-border))",
+          ring: "hsl(var(--sidebar-ring))",
         },
       },
       borderRadius: {
